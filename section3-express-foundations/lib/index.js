cb908cbe8fa8d6c11c75ab3601acad70dce9ba78
Igor Silva <danieligord7@hotmail.com>
2022-12-19 16:29:18 -0300

    SECTION 3: Understanding routes concept

--- section3-express-foundations/lib/index.js
+++ section3-express-foundations/lib/index.js
@@ -1,11 +1,20 @@
 "use strict";
-var express = require("express"); //import express
-var app = express(); //instance express to an app const
-app.listen(4000, function (error) {
-    if (error) {
-        console.log('Error: ', error);
-    }
-    else {
-        console.log('Success!');
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express")); //import express
+var app = (0, express_1.default)(); //instance express to an app const
+var port = 4000;
+app.get("/", function (_req, res) {
+    res.send('Você está na root'); //define get route response
+});
+app.get('/contact', function (_req, res) {
+    res.send("Você está na Contact Page");
+});
+app.get('/about', function (_req, res) {
+    res.send("Você está na About Page");
+});
+app.listen(port, function () {
+    console.log("App is running on ".concat(port, " "));
 });
--- section3-express-foundations/src/index.ts
+++ section3-express-foundations/src/index.ts
@@ -1,10 +1,20 @@
-const express = require("express") //import express
-const app = express() //instance express to an app const
+import express, {Request, Response} from 'express' //import express
 
-app.listen(4000, (error: any)=>{
-  if(error){
-    console.log('Error: ', error)
-  }else{
-    console.log('Success!')
-  }
+const app = express() //instance express to an app const
+const port: number = 4000
+
+app.get("/", (_req: Request, res: Response)=>{ //defining get route
+  res.send('Você está na root') //define get route response
+})
+
+app.get('/contact', (_req: Request, res: Response)=>{
+  res.send("Você está na Contact Page")
+})
+
+app.get('/about', (_req: Request, res: Response)=>{
+  res.send("Você está na About Page")
+})
+
+app.listen(port, ()=>{ //setting up the server 
+  console.log(`App is running on ${port} `)
 })
