abcc644a0e93265c08b6e36d6387a0a763229a6a
Igor Silva <danieligord7@hotmail.com>
2022-12-20 14:57:09 -0300

    SECTION 3: Handling with parameters

--- section3-express-foundations/src/index.ts
+++ section3-express-foundations/src/index.ts
@@ -15,6 +15,28 @@ app.get('/about', (_req: Request, res: Response)=>{
   res.send("Você está na About Page")
 })
 
-app.listen(port, ()=>{ //setting up the server 
-  console.log(`App is running on ${port} `)
+app.get('/iae/:name', function(_req: Request, res: Response){ //barra dois pontos e nome, definindo o parâmetro nome
+
+  const response = [`Fala, ${_req.params.name}. Tá aqui o que você queria.`, _req.params]
+
+  if(_req.params.name){
+    res.send(response)
+  }
 })
+
+app.get('/falatu/:info?', (req: Request, res: Response)=>{ //Optional parameter
+  const infoPassada = req.params.info
+  if(infoPassada){
+    res.send('Obrigado pela informação!')
+  }else{
+    res.send('Nos envie alguma informação!')
+  }
+})
+
+try {
+  app.listen(port, ()=>{ //setting up the server 
+    console.log(`App is running on ${port} `)
+  })
+} catch (error) {
+  console.log('Error... ', error)
+}
