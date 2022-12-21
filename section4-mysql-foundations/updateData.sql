8303301a06bf9e9c583564b186a53e1d153b7d1a
Igor Silva <danieligord7@hotmail.com>
2022-12-21 16:42:52 -0300

    SECTION 4: Exploring SQL. UPDATE.

---
+++ section4-mysql-foundations/updateData.sql
@@ -0,0 +1,4 @@
+-- To update one register in a table
+UPDATE <table_name> SET <campo> = "Nova info" WHERE <campo> = "Info atual";
+-- In this case
+UPDATE testusers SET usrname = "Kawany Santos" WHERE usrname = "Kawany Mendes";
