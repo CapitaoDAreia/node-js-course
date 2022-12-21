0aea592eeebe651909eaaaf10fd72e90b84278d5
Igor Silva <danieligord7@hotmail.com>
2022-12-21 16:36:29 -0300

    SECTION 4: Exploring SQL. DELETE.

---
+++ section4-mysql-foundations/deleteData.sql
@@ -0,0 +1,6 @@
+-- To delete one register in a table
+DELETE FROM <table_name> WHERE condition;
+-- In this case
+DELETE FROM <table_name> WHERE usrname = 'Igor';
+
+-- Do not run DELETE FROM without WHERE!!! 
