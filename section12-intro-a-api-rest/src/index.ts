46923323261469cbfcf5261c9213bcd9276e52a1
Igor Silva <danieligord7@hotmail.com>
2023-01-09 22:32:14 -0300

    SECTION 12: Intro to REST APIs, listing data on a get route.

--- section12-intro-a-api-rest/src/index.ts
+++ section12-intro-a-api-rest/src/index.ts
@@ -1,16 +1,28 @@
-import express, {Request, Response} from 'express';
-import DB from './DB/MockedDB';
+import express, { Request, Response } from "express";
+import DB from "./DB/MockedDB";
 
 const PORT = 80;
 const app = express();
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get(`/`, (req: Request, res: Response)=>{
-    res.send(DB)
-})
+app.get(`/`, (_req: Request, res: Response) => {
+  const avaiableRoutes = [
+    { get: ["/", "/games"] },
+    { post: [] },
+    { patch: [] },
+    { put: [] },
+    { delete: [] },
+  ];
 
+  res.send(avaiableRoutes);
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is listen on PORT ${PORT}`)
-})
+app.get(`/games`, (_req: Request, res: Response) => {
+  res.statusCode = 200;
+  res.json(DB.games);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listen on PORT ${PORT}`);
+});
